1207bd7e1b70befbed1239cbc16ef0b9aee3000a
Derek Nguyen <dereknguyen123@outlook.com>
2020-12-01 16:38:47 -0500

    Commenting on functions.
    Just clarifying on functions for good practice.

--- Challenge 1/sum2020.py
+++ Challenge 1/sum2020.py
@@ -4,11 +4,16 @@ def sum2020(nums, target):
     :type target = int
     :rtype: int
     """
+    # Create a set to store any values we looked at already
     checked = set()
     for index, num in enumerate(nums):
+        # Complement is the required number to reach the target
         complement = target - int(num)
+        # See if we have already found the complement in the list
         if(complement in checked):
+            # If we have, return the product of the two numbers
             return (int(num) * complement)
+        # Otherwise, add the current number to the checked numbers
         else:
             checked.add(int(num))
 
--- Challenge 1/sum2020Two.py
+++ Challenge 1/sum2020Two.py
@@ -4,23 +4,40 @@ def sum2020Helper(nums, target):
     :type target = int
     :rtype: int
     """
+    # Create a set to store any values we looked at already
     checked = set()
     for index, num in enumerate(nums):
+        # Complement is the required number to reach the target
         complement = target - int(num)
+        # See if we have already found the complement in the list
         if(complement in checked):
+            # If we have, return the product of the two numbers
             return (int(num) * complement)
+        # Otherwise, add the current number to the checked numbers
         else:
             checked.add(int(num))
 
+
 def sum2020(nums, target):
+    """
+    :type nums = List[str]
+    :type target = int
+    :rtype: int
+    """
+    # Iterate through the list of input
     for index, num in enumerate(nums):
+        # If there are only two numbers left in the list
         if(index == len(nums)-2):
+            # There is no solution
             return null
-        twoSum = sum2020Helper(nums[index+1:len(nums)], target - int(num))
+        # Complement is the second integer such that the current num + that num
+        complement = target - int(num)
+        # Use sum2020Helper to find two numbers in the remainder of the list
+        twoSum = sum2020Helper(nums[index+1:len(nums)], complement)
         if(twoSum):
             return int(num) * twoSum
-            
-    
+
+
 inputFile = open("input.txt", "r")
 inputLines = inputFile.read().splitlines()
 inputFile.close()

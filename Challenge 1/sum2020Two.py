2e40417db4018a91dc0644e159d3328b1c2a008a
Derek Nguyen <dereknguyen123@outlook.com>
2020-12-01 12:57:41 -0500

    Found the product of 3 numbers that summed to 2020
    Iterated through every number in the list, did the 2sum on the rest of it.

---
+++ Challenge 1/sum2020Two.py
@@ -0,0 +1,28 @@
+def sum2020Helper(nums, target):
+    """
+    :type nums = List[str]
+    :type target = int
+    :rtype: int
+    """
+    checked = set()
+    for index, num in enumerate(nums):
+        complement = target - int(num)
+        if(complement in checked):
+            return (int(num) * complement)
+        else:
+            checked.add(int(num))
+
+def sum2020(nums, target):
+    for index, num in enumerate(nums):
+        if(index == len(nums)-2):
+            return null
+        twoSum = sum2020Helper(nums[index+1:len(nums)], target - int(num))
+        if(twoSum):
+            return int(num) * twoSum
+            
+    
+inputFile = open("input.txt", "r")
+inputLines = inputFile.read().splitlines()
+print(type(inputLines))
+result = sum2020(inputLines, 2020)
+print(result)
